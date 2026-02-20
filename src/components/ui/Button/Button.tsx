0f443d0aba349df39e02c77580395bea59c33aeb
LGalushka <liliya2615@gmail.com>
2026-02-20 17:52:03 +0300

    chore: format project with prettier

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -14,13 +14,19 @@ export const Button = ({
     'px-lg py-sm rounded-box-sm font-medium transition-all duration-100 flex items-center justify-center cursor-pointer active:scale-95 disabled:opacity-50 disabled:cursor-not-allowed';
 
   const variants = {
-    primary: 'bg-primary hover:bg-primary-hover text-text-light shadow-sm',
-    secondary: 'bg-secondary hover:bg-secondary-hover text-text-primary',
-    danger: 'bg-danger hover:bg-danger-hover text-text-light',
+    primary:
+      'bg-primary hover:bg-primary-hover text-text-light shadow-sm',
+    secondary:
+      'bg-secondary hover:bg-secondary-hover text-text-primary',
+    danger:
+      'bg-danger hover:bg-danger-hover text-text-light',
   };
 
   return (
-    <button className={`${baseStyles} ${variants[variant]} ${className}`} {...props}>
+    <button
+      className={`${baseStyles} ${variants[variant]} ${className}`}
+      {...props}
+    >
       {children}
     </button>
   );
--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -5,20 +5,22 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: boolean;
 }
 
-export const Input = ({ label, error, className = '', ...props }: InputProps) => {
+export const Input = ({
+  label,
+  error,
+  className = '',
+  ...props
+}: InputProps) => {
   return (
-    <div className="flex flex-col gap-xs w-full">
-      {label && <label className="text-sm text-text-secondary ml-xs">{label}</label>}
+    <div className="gap-xs flex w-full flex-col">
+      {label && (
+        <label className="text-text-secondary ml-xs text-sm">
+          {label}
+        </label>
+      )}
 
       <input
-        className={`
-        bg-input text-text-primary p-md rounded-box-xs border outline-hidden transition-all duration-200 placeholder:text-text-secondary/50
-        /*Если нет ошибка - обычная рамка, при фокусе -синяя*/
-        ${error ? 'border-danger focus:border-danger' : 'border-border-color focus:border-primary-hover'}
-        /*Добавим легкое свечение при фокусе*/
-        focus:right-2 focus:ring-primary/20
-        ${className}        
-        `}
+        className={`bg-input text-text-primary p-md rounded-box-xs placeholder:text-text-secondary/50 /*Если нет ошибка - обычная рамка, при фокусе -синяя*/ border outline-hidden transition-all duration-200 ${error ? 'border-danger focus:border-danger' : 'border-border-color focus:border-primary-hover'} /*Добавим легкое свечение при фокусе*/ focus:ring-primary/20 focus:right-2 ${className} `}
         {...props}
       />
     </div>

a3969e1bf5d5b72f664ea748663cdcc5c6ce0843
LGalushka <liliya2615@gmail.com>
2026-02-21 11:17:47 +0300

    feat: add useLocalStorage and useHabit hooks for state management

---
+++ src/pages/HabitTracker/hooks/useHabits.ts
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import { useLocalStorageHabit } from './useLocalStorageHabit';
+
+useState;
+interface Habit {
+  id: string;
+  name: string;
+  completedDates: string[];
+}
+
+export const useHabits = () => {
+  const [habits, setHabits] = useLocalStorageHabit<Habit[]>(
+    'habit',
+    []
+  );
+
+  //добавление
+  const addHabit = (name: string) => {
+    if (!name.trim()) return;
+    const newHabit: Habit = {
+      id: crypto.randomUUID(),
+      name,
+      completedDates: [],
+    };
+    setHabits((prev) => [...prev, newHabit]);
+  };
+
+  //переключение
+  const toggleHabit = (habitId: string) => {
+    const today = new Date().toISOString().split('T')[0];
+    setHabits((prev) =>
+      prev.map((habit) => {
+        if (habit.id !== habitId) return habit;
+        const hasToday =
+          habit.completedDates.includes(today);
+        return {
+          ...habit,
+          completedDates: hasToday
+            ? habit.completedDates.filter(
+                (d) => d !== today
+              )
+            : [...habit.completedDates, today],
+        };
+      })
+    );
+  };
+
+  const getStreak = (dates: string[]): number => {
+    if (dates.length === 0) return 0;
+
+    const today = new Date().toISOString().split('T')[0];
+    const yesterday = new Date();
+
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const yesterdayStr = yesterday
+      .toISOString()
+      .split('T')[0];
+    if (
+      !dates.includes(today) &&
+      !dates.includes(yesterdayStr)
+    )
+      return 0;
+
+    let streak = 0;
+
+    let curr = new Date();
+
+    if (!dates.includes(today)) {
+      curr.setDate(curr.getDate() - 1);
+    }
+    while (true) {
+      const s = curr.toISOString().split('T')[0];
+      if (dates.includes(s)) {
+        streak++;
+        curr.setDate(curr.getDate() - 1);
+      } else {
+        break;
+      }
+    }
+    return streak;
+  };
+
+  return { habits, addHabit, toggleHabit, getStreak };
+};

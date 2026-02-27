c8b057d88bcceb978aef07e695e782bd47aa14b4
LGalushka <liliya2615@gmail.com>
2026-02-27 16:12:43 +0300

    feat: add CoinsTable, CoinsTableRow and MarketCapChart components

--- src/pages/CryptoTrack/components/CryptoTrack/CryptoTrack.tsx
+++ src/pages/CryptoTrack/components/CryptoTrack/CryptoTrack.tsx
@@ -5,6 +5,7 @@ import type { Coin } from '../../types/crypto';
 import { CoinCard } from '../CoinCard/CoinCard';
 import CryptoHeader from '../CryptoHeader/CryptoHeader';
 import { MarketCapChart } from '../MarketCapChart';
+import { CoinsTable } from '../CoinsTable';
 
 export const CryptoTrack = () => {
   const [search, setSearch] = useState<string>('');
@@ -13,6 +14,8 @@ export const CryptoTrack = () => {
     (signal) => fitchCoins({ signal })
   );
 
+  const [favorites, setFavorites] = useState<string[]>([]);
+
   const allCoins = data ?? [];
   const filteredCoins = search
     ? allCoins.filter(
@@ -20,9 +23,17 @@ export const CryptoTrack = () => {
           coin.name.toLowerCase().includes(search.trim().toLowerCase()) ||
           coin.symbol.toLowerCase().includes(search.trim().toLowerCase())
       )
-    : allCoins;
+    : allCoins.slice(0, 10);
 
   const bitcoin = filteredCoins.find((coin) => coin.id === 'bitcoin');
+
+  const toggleFavorite = (coinID: string) => {
+    setFavorites((prev) =>
+      prev.includes(coinID)
+        ? prev.filter((id) => id !== coinID)
+        : [...prev, coinID]
+    );
+  };
   return (
     <div className="m-6">
       <CryptoHeader
@@ -40,6 +51,12 @@ export const CryptoTrack = () => {
         {bitcoin ? <CoinCard crypto={bitcoin} /> : <p>Монета не найдена</p>}
       </div>
       <MarketCapChart coins={data ?? []} />
+
+      <CoinsTable
+        coins={filteredCoins}
+        favorites={favorites}
+        onToggleFavorite={toggleFavorite}
+      />
     </div>
   );
 };

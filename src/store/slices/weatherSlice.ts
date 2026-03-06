47f4bce87b8054aa61f3172cabf63d71fe2052e7
LGalushka <liliya2615@gmail.com>
2026-03-06 21:01:19 +0300

    feat(weather): add weatherSlice

---
+++ src/store/slices/weatherSlice.ts
@@ -0,0 +1,77 @@
+import {
+  createAsyncThunk,
+  createSlice,
+  type PayloadAction,
+} from '@reduxjs/toolkit';
+import type { WeatherResponse } from '../../pages/WeatherApp/types/weather';
+
+import { fetchWeatherData } from '../../pages/WeatherApp/api';
+
+export const searchWeather = createAsyncThunk<
+  WeatherResponse,
+  { city: string; days?: number },
+  { rejectValue: string }
+>('weather/search', async ({ city, days = 3 }, { rejectWithValue, signal }) => {
+  try {
+    return await fetchWeatherData(city, days, { signal });
+  } catch (err) {
+    if (err instanceof DOMException && err.name === 'AbortError') throw err;
+    return rejectWithValue(
+      err instanceof Error ? err.message : 'Ошибка загрузки'
+    );
+  }
+});
+
+interface WeatherState {
+  data: WeatherResponse | null;
+  loading: boolean;
+  error: string | null;
+  city: string;
+}
+
+const initialState: WeatherState = {
+  data: null,
+  loading: false,
+  error: null,
+  city: '',
+};
+
+const weatherSlice = createSlice({
+  name: 'weather',
+  initialState,
+  reducers: {
+    setCity: (state, action: PayloadAction<string>) => {
+      state.city = action.payload;
+    },
+    claerError: (state) => {
+      state.error = null;
+    },
+    clearWeather: (state) => {
+      state.data = null;
+      state.error = null;
+      state.loading = false;
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(searchWeather.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(searchWeather.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(searchWeather.rejected, (state, action) => {
+        state.loading = false;
+        if (action.error.name !== 'AbortError') {
+          state.error = action.payload ?? 'Ошибка загрузки';
+          state.data = null;
+        }
+      });
+  },
+});
+
+export const { setCity, claerError, clearWeather } = weatherSlice.actions;
+export default weatherSlice.reducer;
